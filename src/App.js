987ae526bb5d10e759eddb0df12ecb28edd2eaef
Bruno Campos <brunosopmac@gmail.com>
2022-10-04 18:50:18 -0300

    prevent re-render with useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-function App() {
+import { useState, useRef } from "react";
+import FormInput from "./components/FormInput";
+import "./App.css";
+
+const App = () => {
+  //const [username, setUsername] = useState('');
+  //const usernameRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="app">
+      <form onSubmit={handleSubmit}>
+        <FormInput placeholder="Username" />
+        <FormInput placeholder="Email" />
+        <FormInput placeholder="Full Name" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+import "./formInput.css"
+
+const FormInput = (props) => {
+    return (
+        <div className="formInput">
+            <input
+                placeholder={props.placeholder}
+            />
+        </div>
+    )
+}
+
+export default FormInput
